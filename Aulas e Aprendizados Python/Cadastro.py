fb21e852815fe871015c8412de9968eccbf7e4d2
Guilherme Pinheiro dos Santos <guilherme.psantos.dev@gmail.com>
2024-10-26 13:20:14 -0300

    Add mudanças

--- Aulas e Aprendizados Python/Cadastro.py
+++ Aulas e Aprendizados Python/Cadastro.py
@@ -1,7 +1,24 @@
 print("****************************************************************************************************************************************************************************************")
 print('\n')
-def sexo ():
-    x = input('Qual o seu sexo: ')
-    if x == 'Homem':
-        print('')
+
+def definir_sexo ():
+    sexo = input('Qual o seu sexo: ')
+    if sexo == 'Homem' or x == 'homem':
+        return print('Seja Bem-Vindo')
+    elif sexo == 'Mulher' or x == 'mulher':
+        return print('Seja Bem-Vinda')
+
+def definir_nome():
+    nome = input('Digite seu nome: ')
+    return print('Seu nome meu inserido no cadastro')
+
+def definir_idade():
+    idade = int(input('Digite sua idade: '))
+    return print('Sua idade foi inserida no cadastro') 
+
+print('Veja se seuus dados estão corretos antes de continuar ')
+
 print("****************************************************************************************************************************************************************************************")
+
+
+#Criar um mathplotlib com esses dados
